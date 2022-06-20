7aa1caf32794e6f77c89ac69734c4ee830be7fac
vianfitri <viancukiep@gmail.com>
2022-06-20 15:15:25 +0700

    Add splash layout

--- app/src/main/java/com/vianfitri/githubuserapp3/ui/splash/Splash.kt
+++ app/src/main/java/com/vianfitri/githubuserapp3/ui/splash/Splash.kt
@@ -1,12 +1,33 @@
 package com.vianfitri.githubuserapp3.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vianfitri.githubuserapp3.MainActivity
 import com.vianfitri.githubuserapp3.R
+import kotlinx.coroutines.*
 
 class Splash : AppCompatActivity() {
+    val activityScope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        activityScope.launch {
+            delay(DELAY_TIME)
+            val intent = Intent(this@Splash, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onPause() {
+        activityScope.cancel()
+        super.onPause()
+    }
+
+    companion object {
+        const val DELAY_TIME = 2000L
     }
 }
