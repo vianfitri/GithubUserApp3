3e2e0ddc8fe2520e4522e9df75205a428dbb62b8
vianfitri <viancukiep@gmail.com>
2022-06-20 14:25:22 +0700

    First Commit

---
+++ app/src/main/java/com/vianfitri/githubuserapp3/ui/splash/Splash.kt
@@ -0,0 +1,12 @@
+package com.vianfitri.githubuserapp3.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vianfitri.githubuserapp3.R
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
